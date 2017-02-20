8309bb2b76d861dd62104fc6c0c2c21918af4022
lobita <jenny.karcevas@gmail.com>
2017-02-20 12:48:51 -0300

    pagina nosotros, envio email,agrego imagenes,borro inecesario

--- Colchoneria/Properties/AssemblyInfo.cs
+++ Colchoneria/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // La información general sobre un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie los valores de estos atributos para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("Colchoneria")]
+[assembly: AssemblyTitle("Colchoneria Bekar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
